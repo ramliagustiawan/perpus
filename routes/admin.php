60d352f31886a8ad714089119d40def99b5bc8ee
ramliagustiawan <ramliagustiawan@gmail.com>
2019-10-17 04:28:50 +0800

    menyederhanakan routing author

--- routes/admin.php
+++ routes/admin.php
@@ -2,11 +2,16 @@
 
 Route::get('/', 'HomeController@index')->name('dashboard');
 
-Route::get('/author', 'AuthorController@index')->name('author.index');
-Route::get('/author/create', 'AuthorController@create')->name('author.create');
-Route::post('/author', 'AuthorController@store')->name('author.store');
-Route::get('/author/{author}/edit', 'AuthorController@edit')->name('author.edit');
-Route::put('/author/{author}', 'AuthorController@update')->name('author.update');
-Route::delete('/author/{author}', 'AuthorController@destroy')->name('author.destroy');
+
 
 Route::get('/author/data', 'DataController@authors')->name('author.data');
+
+// Route::get('/author', 'AuthorController@index')->name('author.index');
+// Route::get('/author/create', 'AuthorController@create')->name('author.create');
+// Route::post('/author', 'AuthorController@store')->name('author.store');
+// Route::get('/author/{author}/edit', 'AuthorController@edit')->name('author.edit');
+// Route::put('/author/{author}', 'AuthorController@update')->name('author.update');
+// Route::delete('/author/{author}', 'AuthorController@destroy')->name('author.destroy');
+
+// route Author diatas bisa lebih disederhanakan
+Route::resource('author', 'AuthorController');
