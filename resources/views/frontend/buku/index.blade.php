51c722f36f0948b510d8742156cfafca02595f4f
ramliagustiawan <ramliagustiawan@gmail.com>
2019-10-21 06:41:42 +0800

    mebuat komponen card

--- resources/views/frontend/buku/index.blade.php
+++ resources/views/frontend/buku/index.blade.php
@@ -14,31 +14,12 @@
     <div class="row">
         @foreach ($books as $book)
 
-            <div class="col s12 m6">
-
-                <div class="card horizontal hoverable">
-                    <div class="card-image">
-                            <img src="{{ $book->getCover() }}" height="200px">
-                    </div>
-                    <div class="card-stacked">
-                        <div class="card-content">
-                            <a href="{{ route('buku.show',$book) }}">
-                                <h6>{{ Str::limit($book->title,30) }}</h6>
-                            </a>
-
-                            <p>{{ Str::limit($book->description,50) }}</p>
-                        </div>
-                        <div class="card-action">
-
-                            <form action="{{ route('buku.borrow',$book) }}" method="POST">
-                                @csrf
-                                <input type="submit" value="Pinjam Buku" class="btn red accent-1 right waves-effect waves-light">
-                            </form>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+             {{-- bisa juga pakai include --}}
+        @component('frontend.templates.components.card-book',
+            [
+                'book'=>$book
+            ])
+        @endcomponent
 
         @endforeach
 
--- resources/views/frontend/buku/show.blade.php
+++ resources/views/frontend/buku/show.blade.php
@@ -45,30 +45,12 @@
 <div class="row">
     @foreach ($book->author->books->shuffle()->take(4) as $book)
 
-        <div class="col s12 m6">
+        {{-- bisa juga pakai include --}}
+       @component('frontend.templates.components.card-book',[
+           'book'=>$book
+       ])
+       @endcomponent
 
-            <div class="card horizontal hoverable">
-                <div class="card-image">
-                        <img src="{{ $book->getCover() }}" height="200px">
-                </div>
-                <div class="card-stacked">
-                    <div class="card-content">
-                        <a href="{{ route('buku.show',$book) }}">
-                            <h6>{{ Str::limit($book->title,30) }}</h6>
-                        </a>
-
-                        <p>{{ Str::limit($book->description,50) }}</p>
-                    </div>
-                    <div class="card-action">
-                        <form action="{{ route('buku.borrow',$book) }}" method="POST">
-                            @csrf
-                            <input type="submit" value="Pinjam Buku" class="btn red accent-1 right waves-effect waves-light">
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-        </div>
 
     @endforeach
 
